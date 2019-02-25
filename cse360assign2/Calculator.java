46acc26afba0c4ab6a9fd1bfe2b90bdfff03debf
Kevin McAllister <kmmcalli@asu.edu>
2019-02-24 20:22:11 -0700

    Completed getHistory() method and added relevant documentation to other methods. Also added return value documentation to getHistory() and getTotal().

--- cse360assign2/Calculator.java
+++ cse360assign2/Calculator.java
@@ -10,47 +10,57 @@ public class Calculator
 {
 
     private int total;
+    private String history;
     /** 
-     * Sets the output value of the calculator to zero.
+     * Sets the output value of the calculator to zero and initializes history string.
      */
     public Calculator () 
     {
         total = 0;  // not needed - included for clarity
+        history = total + " ";
     }
     /**
      * Outputs the final value calculated. 
+     * @return The calculated value.
      */
     public int getTotal () 
     {
         return total;
     }
     /**
-     * Adds an integer to the total.
+     * Adds an integer to the total. 
+     * Also updates the history string with a "+" and the integer.
      * @param value The integer to be added.
      */
     public void add (int value) 
     {
         total += value;
+        history += "+ " + value + " ";
     }
     /**
-     * Subtracts an integer from the total.
+     * Subtracts an integer from the total. 
+     * Also updates the history string with a "-" and the integer.
      * @param  value The value to be subtracted from the total.
      */
     public void subtract (int value) 
     {
         total -= value;
+        history += "- " + value + " ";
     }
     /**
-     * Multiplies an integer by the total.
+     * Multiplies an integer by the total. 
+     * Also updates the history string with a "*" and the integer.
      * @param  value The value to be multiplied by the total.
      */
     public void multiply (int value) 
     {
         total *= value;
+        history += "* " + value + " ";
     }
     /**
      * Divides the total by an integer. If the parameter provided is a zero, the function 
-     * instead sets the total to zero.
+     * instead sets the total to zero. 
+     * Also updates the history string with a "/" and the integer.
        * @param  value The value the total is to be divided by.
      */
     public void divide (int value) 
@@ -63,13 +73,14 @@ public class Calculator
         {
             total /= value;
         }
+        history += "/ " + value + " ";
     }
     /**
-     * Returns the series of mathematical operations performed up to this point in string
-     * format.
+     * Outputs the series of mathematical operations performed up to this point as a string.
+     * @return The full equation in String format.
      */
     public String getHistory () 
     {
-        return "";
+        return history;
     }
 }
